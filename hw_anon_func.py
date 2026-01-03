e7ff6beb82822b44acc29fe5bbb3ec51511052ff
Marat Gataullin <thedancerdisco@gmail.com>
2026-01-04 00:36:03 +0300

    анонимн функ

--- hw_anon_func.py
+++ hw_anon_func.py
@@ -4,5 +4,6 @@ students = [
 {"name": "Игорь", "score": 72},
 {"name": "Марина", "score": 91},
 ]
+
 stud = list(filter(lambda x: x["score"]>85, sorted(students, key = lambda x: x["score"] )))
 print(list(map(lambda x: x["name"].upper(), stud)))
